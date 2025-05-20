0d03dce112db63423f19b7d4e6a4b3753930dd00
katharina.starzer@eodc.eu <katharina.starzer@eodc.eu>
2025-05-20 08:49:37 +0200

    new conf

--- docs/conf.py
+++ docs/conf.py
@@ -5,13 +5,9 @@ import sys
 import pathlib
 
 # -- Path setup ----------------------------------------------------------------
-
-# Make _extensions importable
 sys.path.insert(0, os.path.abspath("_extensions"))
 
 # -- Conditional Extension Loading --------------------------------------------
-
-# Load notebook_gallery_generator only when building the central docs/ page
 if pathlib.Path(__file__).parent.resolve().name == "docs":
     extensions = [
         "sphinx.ext.githubpages",
@@ -27,39 +23,44 @@ else:
     ]
 
 # -- Project information -------------------------------------------------------
-project = 'Jupyter Notebook Gallery'
+project = 'DestinE DataLake Lab'
 copyright = '2024, Destination Earth Data Lake'
-author = 'Christoph Reimer <christoph.reimer@eodc.eu>'
+author = 'DestinE Data Lake Team'
 
 # -- General configuration -----------------------------------------------------
-myst_enable_extensions = [
-    "amsmath",
-    "colon_fence",
-    "deflist",
-    "html_image",
-    "dollarmath",
-]
-
-myst_url_schemes = ["http", "https", "mailto"]
-myst_heading_anchors = 3
-nb_execution_mode = 'off'
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "**/.git", ".pixi**", "book/**"]
 
 source_suffix = {
     ".ipynb": "myst-nb",
     ".myst": "myst-nb",
 }
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "**/.git", ".pixi**"]
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "substitution",
+]
+
+myst_url_schemes = ["http", "https", "mailto"]
+myst_heading_anchors = 3
+nb_execution_mode = 'off'
+nb_execution_timeout = 3600
+nb_execution_allow_errors = False
 
 # -- HTML output configuration -------------------------------------------------
 html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
-html_css_files = ['custom.css']
-
+html_title = "DestinE DataLake Lab"
 html_logo = "https://hda.data.destination-earth.eu/ui/images/destination_earth_logo_W.svg"
 html_favicon = "https://hda.data.destination-earth.eu/ui/images/favicon.svg"
-html_title = "DEDL Notebook Gallery"
+html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 html_theme_options = {
     "logo": {
@@ -71,17 +72,30 @@ html_theme_options = {
     "navigation_with_keys": True,
     "show_nav_level": 2,
     "navigation_depth": 2,
-    "collapse_navigation": False,
+    "collapse_navigation": True,
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/destination-earth",
+            "url": "https://github.com/destination-earth/DestinE-DataLake-Lab",
             "icon": "fa-brands fa-square-github",
             "type": "fontawesome",
         }
     ],
 }
 
-# -- Entry point for Sphinx ----------------------------------------------------
-master_doc = 'index'
+# -- Launch button config (Binder/JupyterHub) ----------------------------------
+launch_buttons = {
+    "notebook_interface": "jupyterlab",
+    "binderhub_url": "https://binder-217-71-193-124.nip.io",
+    # "jupyterhub_url": "https://jupyter.central.data.destination-earth.eu/",
+}
 
+# -- Repository link in nav ----------------------------------------------------
+repository = {
+    "url": "https://github.com/destination-earth/DestinE-DataLake-Lab",
+    "branch": "notebook-gallery",
+    # "path_to_book": "book",
+}
+
+# -- Entry point ---------------------------------------------------------------
+master_doc = 'index'
